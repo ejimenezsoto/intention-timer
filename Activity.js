98bd2622e365f78ecde1d4e6774c07145698def5
Phil Lewis <phillewis1985@gmail.com>
2021-11-03 16:54:21 -0600

    Change category button color functionality

--- Activity.js
+++ Activity.js
@@ -2,7 +2,7 @@ class Activity {
   constructor(category, description, minutes, seconds) {
     this.category = category;
     this.description = description;
-    this.minutes = = minutes;
+    this.minutes = minutes;
     this.seconds = seconds;
     this.completed = false;
     this.id = Date.now();
@@ -18,4 +18,4 @@ class Activity {
   }
 }
 
-module.exports = Activity;
+// module.exports = Activity;
